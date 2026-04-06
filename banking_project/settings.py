ccc225dcaad72f6742e3cab0bf3b842b957d92b6
Himanshu Srivastava <hemans681@gmail.com>
2026-04-06 18:18:51 +0530

    changing settings for first deployment

--- banking_project/settings.py
+++ banking_project/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-3v2il2cs&&6)hx@j4ym+z(peio+g@f2o(ry289(+)6xf@r75s+"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
